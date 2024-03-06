a841904746cf61a102fb27f8362f0d1f0af71809
Zain <72197346+zain-mzb@users.noreply.github.com>
2024-03-06 13:19:59 +0900

    Sub Array Division

---
+++ subArrayDivision.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(sp, dp, mp) {
+    // Write your code here
+
+    let sum = 0;
+    let countArr = [];
+    for (let i = 0; i < sp.length; i++) {
+        sum = sp[i]
+        for (let j = 1; j < mp; j++) {
+            if (sp[i+j] < sp.length - 1) {
+                sum = sum + sp[i+j]
+            }
+        }
+        if (sum == dp) {
+            countArr.push(sum)
+        }
+    }
+
+    return countArr.length;
+
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
